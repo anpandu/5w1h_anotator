5df64a1096c0e40eee5343b5c9d5b8a380a7753d
anpandu <anpandumail@gmail.com>
2014-09-26 14:56:14 +0700

    migrate 5w1hs

---
+++ app/database/migrations/2014_09_26_074703_create_5w1hs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Create5w1hsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('5w1hs', function(Blueprint $table) {
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->integer('article_id')->unsigned();
+			$table->string('what');
+			$table->string('who');
+			$table->string('where');
+			$table->string('when');
+			$table->string('why');
+			$table->string('how');
+			$table->dateTime('created_at');
+			$table->dateTime('updated_at');
+
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('article_id')->references('id')->on('articles');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		$table->dropForeign('5w1hs_user_id_foreign');
+		$table->dropForeign('5w1hs_article_id_foreign');
+    	Schema::drop('5w1hs');
+	}
+
+}
