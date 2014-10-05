14979c1612774225be1ab0ad43861bc269b5b6c7
anpandu <anpandumail@gmail.com>
2014-10-05 18:43:11 +0700

    guidance

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.default')
 @section('content')
-	i am the home page
+    <div class="col-sm-1"></div>
+    <div class="col-sm-10">
+    	<h2>Panduan Anotasi 5W1H</h2>
+    	<ol>
+    		<li>Tugas anda adalah membaca berita pada kolom sebelah kiri.</li>
+    		<li>Selanjutnya ketik informasi 5w1h pada form sebelah kanan.</li>
+    		<li>Harap ketik informasi sesuai berita, tidak disingkat. Jika info berbentuk kalimat, tak boleh ada loncat kata.</li>
+    		<li>Setelah selesai mengisi akan muncul highlight pada paragraf berita.</li>
+    		<li>Jika highlight tidak muncul, harap ulangi sampai muncul.</li>
+    		<li>Form dikosongkan apabila dianggap tidak ditemukan adanya informasi.</li>
+    		<li><a href='{{url("article/list")}}'>Mulai</a></li>
+    	</ol>
+    </div>
+    <div class="col-sm-1"></div>
 @stop
