911d8d1480eb44a5d97b86074a001808c9e0f586
anpandu <anpandumail@gmail.com>
2014-10-05 14:10:06 +0700

    highlight info 5w1h

--- app/views/pages/info/create.blade.php
+++ app/views/pages/info/create.blade.php
@@ -8,7 +8,8 @@
         <div class="row">
             <div class="col-sm-6">
                 <h1>{{ $article->title }}</h1>
-                <p>{{ str_replace("\n", "<br>", $article->text) }}</p>
+                <input id="newsContent" type="hidden" value='{{ $article->text }}'>
+                <p id="newsContentFix">{{ str_replace("\n", "<br>", $article->text) }}</p>
             </div>
             <div class="col-sm-1"></div>
             <div class="col-sm-5">
@@ -67,20 +68,71 @@
     {{ HTML::script('packages/jquery-ui/jquery-ui.js') }}
 
     <script type="text/javascript">
+
+        var hl = {};
+
+        var getIdxSentenceInWordArr = function (word_arr, sentence) {
+            var sentence_arr = sentence.split(" ");
+            var j = 0;
+            var res = [];
+            for (var i = 0; i < word_arr.length; i++) {
+                var word = word_arr[i];
+                if (word==sentence_arr[j]) {
+                    j += 1;
+                    res.push(i);
+                }
+            };
+            return res.slice(0);
+        }
+
+        var highlightSentence = function (sentence, info_id) {
+            var content = $('#newsContent').val();
+            var content_arr = content.replace(/\n/g, " \n ").split(" ");
+
+            hl[info_id] = getIdxSentenceInWordArr(content_arr, sentence);
+
+            var info = [];
+            info.push(["inputWhat", "hl_what"]);
+            info.push(["inputWho", "hl_who"]);
+            info.push(["inputWhere", "hl_where"]);
+            info.push(["inputWhen", "hl_when"]);
+            info.push(["inputWhy", "hl_why"]);
+            info.push(["inputHow", "hl_how"]);
+
+            for (var j = 0; j < info.length; j++) {
+                var inp = info[j][0];
+                var cls = info[j][1];
+                if(inp in hl) {
+                    for (var i = 0; i < hl[inp].length; i++) {
+                        content_arr[hl[inp][i]] = "<span class='"+cls+"'>"+content_arr[hl[inp][i]]+"</span>";
+                    }
+                }
+            }
+            var contentfix = content_arr.join(" ").replace(/\n/g, "<br>");
+            contentfix = contentfix.replace(/<\/span/g," <\/span");
+            contentfix = contentfix.replace(/span> <span/g,"span><span");
+            $("#newsContentFix").html(contentfix);
+        }
+
+
+
         $(function() {
             var option = function (input) {
                 return {
                     source: '{{url("autocomplete/".$article->id)}}',
                     delay: 300,
-                    select: function(value, data){$(input).autocomplete( "search", $(input).val());}
+                    select: function(event, ui){
+                        $("#"+input).autocomplete( "search", $("#"+input).val()); 
+                        highlightSentence(ui['item']['value'], input);
+                    }
                 }
             }
-            $( "#inputWhat" ).autocomplete(option("#inputWhat"));
-            $( "#inputWho" ).autocomplete(option("#inputWho"));
-            $( "#inputWhere" ).autocomplete(option("#inputWhere"));
-            $( "#inputWhen" ).autocomplete(option("#inputWhen"));
-            $( "#inputWhy" ).autocomplete(option("#inputWhy"));
-            $( "#inputHow" ).autocomplete(option("#inputHow"));
+            $( "#inputWhat" ).autocomplete(option("inputWhat"));
+            $( "#inputWho" ).autocomplete(option("inputWho"));
+            $( "#inputWhere" ).autocomplete(option("inputWhere"));
+            $( "#inputWhen" ).autocomplete(option("inputWhen"));
+            $( "#inputWhy" ).autocomplete(option("inputWhy"));
+            $( "#inputHow" ).autocomplete(option("inputHow"));
         });
     </script>
 @stop
