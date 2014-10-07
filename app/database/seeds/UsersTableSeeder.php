73050242d24c0f95b77fcdd73ef063c82e82102c
anpandu <anpandumail@gmail.com>
2014-10-07 22:32:32 +0700

    seed new usernames

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -12,18 +12,21 @@ class UsersTableSeeder extends Seeder {
             'username' => 'guess',
             'password' => Hash::make('guess'),
         ));
-        User::create(array(
-            'username' => 'alpha',
-            'password' => Hash::make('alpha'),
-        ));
-        User::create(array(
-            'username' => 'bravo',
-            'password' => Hash::make('bravo'),
-        ));
-        User::create(array(
-            'username' => 'charlie',
-            'password' => Hash::make('charlie'),
-        ));
+
+        User::create(array('username' => 'alpha', 'password' => Hash::make('alpha'), ));
+        User::create(array( 'username' => 'bravo', 'password' => Hash::make('bravo'), ));
+        User::create(array( 'username' => 'charlie', 'password' => Hash::make('charlie'), ));
+        User::create(array( 'username' => 'abc', 'password' => Hash::make('abc'), ));
+
+        User::create(array('username' => 'delta', 'password' => Hash::make('delta'), ));
+        User::create(array( 'username' => 'echo', 'password' => Hash::make('echo'), ));
+        User::create(array( 'username' => 'foxtrot', 'password' => Hash::make('foxtrot'), ));
+        User::create(array( 'username' => 'def', 'password' => Hash::make('def'), ));
+
+        User::create(array('username' => 'golf', 'password' => Hash::make('golf'), ));
+        User::create(array( 'username' => 'hotel', 'password' => Hash::make('hotel'), ));
+        User::create(array( 'username' => 'india', 'password' => Hash::make('india'), ));
+        User::create(array( 'username' => 'ghi', 'password' => Hash::make('ghi'), ));
     }
 
 }
