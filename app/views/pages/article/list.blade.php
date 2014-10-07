6501371384e1e861df98f7f47e1de71859c16646
anpandu <anpandumail@gmail.com>
2014-10-07 23:24:03 +0700

    'see all' link in article list

--- app/views/pages/article/list.blade.php
+++ app/views/pages/article/list.blade.php
@@ -6,17 +6,21 @@
             <tr>
                 <td>ID</td>
                 <td>Article</td>
-                <td>5W1H Status</td>
+                <td>Action</td>
+                <td>5W1H</td>
             </tr>
             @foreach ($articles as $article)
                 <tr>
                     <td><p>{{ $article->id }}</p></td>
                     <td><a href="{{url('/article/'.$article->id);}}">{{ $article->title }}</a></td>
-                    @if (in_array($article->id, $owned_article_ids))
-                        <td><a href="{{url('/info/edit/'.$article->id);}}">Edit</a></td>
-                    @else
-                        <td><a href="{{url('/info/create/'.$article->id);}}">Add</a></td>
-                    @endif
+                    <td>
+                        @if (in_array($article->id, $owned_article_ids))
+                            <a href="{{url('/info/edit/'.$article->id);}}">Edit</a>
+                        @else
+                            <a href="{{url('/info/create/'.$article->id);}}">Add</a>
+                        @endif
+                    </td>
+                    <td><a href="{{url('/article/'.$article->id).'/all';}}">See All</a></td>
                 </tr>
             @endforeach
         </table>
