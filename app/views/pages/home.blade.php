92d0eee4be08d285744d2de31d01638de600939a
anpandu <anpandumail@gmail.com>
2014-11-03 00:14:51 +0700

    panduan w/ gambar

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -6,6 +6,7 @@
     	<ol>
             <li>Tugas anda adalah membaca berita pada kolom sebelah kiri dan mengisi informasi 5w1h.</li>
     		<li>Untuk mengisi form 5w1h dengan kata/kalimat di paragraf, klik karakter pertama lalu klik karakter terakhir pada kata/kalimat yang diinginkan pada paragraf.</li>
+            <li><img src='/images/panduananotasi.png'></li>
             <li>Akan keluar pop-up di depan form dan ada enam tombol: what, who, when, where, dan how.</li>
             <li>Klik salah satu tombol untuk mengisi form yang sesuai.</li>
             <li>Jika tidak menemukan info 5w1h boleh dikosongkan.</li>
