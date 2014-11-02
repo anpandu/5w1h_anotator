e4ff1d788837e640cc288ffa7c040bac6e641a60
anpandu <anpandumail@gmail.com>
2014-11-02 23:51:34 +0700

    update panduan

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -4,12 +4,12 @@
     <div class="col-sm-10">
     	<h2>Panduan Anotasi 5W1H</h2>
     	<ol>
-    		<li>Tugas anda adalah membaca berita pada kolom sebelah kiri.</li>
-    		<li>Selanjutnya ketik informasi 5w1h pada form sebelah kanan.</li>
-    		<li>Harap ketik informasi sesuai berita, tidak disingkat. Jika info berbentuk kalimat, tak boleh ada loncat kata.</li>
-    		<li>Setelah selesai mengisi akan muncul highlight pada paragraf berita.</li>
-    		<li>Jika highlight tidak muncul, harap ulangi sampai muncul.</li>
-    		<li>Form dikosongkan apabila dianggap tidak ditemukan adanya informasi.</li>
+            <li>Tugas anda adalah membaca berita pada kolom sebelah kiri dan mengisi informasi 5w1h.</li>
+    		<li>Untuk mengisi form 5w1h dengan kata/kalimat di paragraf, klik karakter pertama lalu klik karakter terakhir pada kata/kalimat yang diinginkan pada paragraf.</li>
+            <li>Akan keluar pop-up di depan form dan ada enam tombol: what, who, when, where, dan how.</li>
+            <li>Klik salah satu tombol untuk mengisi form yang sesuai.</li>
+            <li>Jika tidak menemukan info 5w1h boleh dikosongkan.</li>
+            <li>N.B. : mengisi form dengan ketik manual tidak dianjurkan.</li>
     		<li><a href='{{url("article/list")}}'>Mulai</a></li>
     	</ol>
     </div>
